ed60b1ee355e273949fed55e0e114b0d740c447a
codertcl <2635633080@qq.com>
2023-12-09 16:00:59 +0800

    feat(12/9):双指针

--- 晓晨/二数组/16. 最接近的三数之和/1.js
+++ 晓晨/二数组/16. 最接近的三数之和/1.js
@@ -5,15 +5,24 @@
  */
 var threeSumClosest = function (nums, target) {
   nums = nums.sort((a, b) => a - b)
-  let res = nums[0] + nums[1] + nums[2]
-  for (let i = 1; i < nums.length - 2; i++) {
-    const sum = nums[i] + nums[i + 1] + nums[i + 2]
-    let a = Math.abs(target - sum), b = Math.abs(target - res)
-    res = a < b ? sum : res
+  let res = Number.MAX_SAFE_INTEGER
+  for (let i = 0; i < nums.length; i++) {
+    let l = i + 1, r = nums.length - 1
+    while (l < r) {
+      const sum = nums[l] + nums[i] + nums[r]
+      if (Math.abs(sum - target) < Math.abs(res - target)) {
+        res = sum
+      }
+      if (sum < target) {
+        l++
+      } else if (sum > target) {
+        r--
+      } else return sum
+    }
   }
   return res
 };
 
-console.log(threeSumClosest([-1,2,1,-4], 1)) // 2
+console.log(threeSumClosest([-1, 2, 1, -4], 1)) // 2
 console.log(threeSumClosest([0, 0, 0], 1)) // 0
-console.log(threeSumClosest([4,0,5,-5,3,3,0,-4,-5], -2)) // -2 [0, -5, 3]
+console.log(threeSumClosest([4, 0, 5, -5, 3, 3, 0, -4, -5], -2)) // -2 [0, -5, 3]
